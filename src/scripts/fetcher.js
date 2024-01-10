306ad4e312a1ddfe1fa456032294ba977dd5148e
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-01-10 12:41:23 -0500

    Update BaseLayout and add new blog pages

---
+++ src/scripts/fetcher.js
@@ -0,0 +1,100 @@
+// TODO Add error handling
+// TODO Setup to get all posts, right now it only gets 5
+// TODO Look into using GraphQL client
+export async function getAllPosts() {
+  const postsRequest = `  
+      query allPosts {  
+          publication(host: "blog.stephcrown.com") {  
+            title  
+            posts(first: 5) {  
+              pageInfo {  
+                hasNextPage  
+                endCursor  
+              }  
+              edges {  
+                node {  
+                  author {  
+                    name  
+                    profilePicture  
+                  }  
+                  title  
+                  subtitle  
+                  brief  
+                  slug  
+                  coverImage {  
+                    url  
+                  }  
+                  tags {  
+                    name  
+                    slug  
+                  }  
+                  publishedAt  
+                  readTimeInMinutes  
+                }  
+              }  
+            }  
+          }  
+        }
+      `;
+
+  try {
+    const res = await fetch("https://gql.hashnode.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: postsRequest }),
+    });
+
+    if (res.status >= 400) {
+      throw new Error("Server responded with a status of " + res.status);
+    }
+
+    const { data } = await res.json();
+
+    // console.log(data);
+    return data;
+  } catch (err) {
+    // console.log(err);
+    return err;
+  }
+}
+
+export async function getPost(slug) {
+  console.log(typeof slug);
+  const postsRequest = `
+  query allPosts {
+    publication(host: "blog.stephcrown.com") {
+      title
+      post(slug: "${slug}") {
+        title
+        slug
+        content {
+          markdown
+          html
+        }
+      }
+    }
+  }
+  `;
+
+  try {
+    const res = await fetch("https://gql.hashnode.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: postsRequest }),
+    });
+
+    if (res.status >= 400) {
+      throw new Error("Server responded with a status of " + res.status);
+    }
+
+    const { data } = await res.json();
+
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
