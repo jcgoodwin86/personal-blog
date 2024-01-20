32d89205cbd80ed927b24555e1af909b2420b964
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-01-20 17:57:46 -0500

    Update ESLint configuration and tag pages
    Made changes to the tag pages ([tag].astro and index.astro) by importing and using the getPosts and getTagsPosts functions from the fetcher script. These changes allow for dynamic generation of paths and fetching of posts based on tags.

--- src/scripts/fetcher.js
+++ src/scripts/fetcher.js
@@ -60,6 +60,65 @@ export async function getPosts(numberOfPosts = 3) {
   }
 }
 
+export async function getTagsPosts(tag, numberOfPosts = 3) {
+  const postsRequest = `  
+      query allPosts {  
+          publication(host: "blog.stephcrown.com") {  
+            title  
+            posts(first: ${numberOfPosts}, filter: {tagSlugs: ["${tag}"]}) {  
+              pageInfo {  
+                hasNextPage  
+                endCursor  
+              }  
+              edges {  
+                node {  
+                  author {  
+                    name  
+                    profilePicture  
+                  }  
+                  title  
+                  subtitle  
+                  brief  
+                  slug  
+                  coverImage {  
+                    url  
+                  }  
+                  tags {  
+                    name  
+                    slug  
+                  }  
+                  publishedAt  
+                  readTimeInMinutes  
+                }  
+              }  
+            }  
+          }  
+        }
+      `;
+
+  try {
+    const res = await fetch("https://gql.hashnode.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: postsRequest }),
+    });
+
+    if (res.status >= 400) {
+      throw new Error("Server responded with a status of " + res.status);
+    }
+
+    const { data } = await res.json();
+
+    // console.log(data);
+    return data;
+  } catch (err) {
+    // console.log(err);
+    return err;
+  }
+}
+
 export async function getPost(slug) {
   console.log(typeof slug);
   const postsRequest = `
